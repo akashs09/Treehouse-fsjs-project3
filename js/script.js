3639c880a8c287fce88f7219fa8bd54a32a97a23
akashs90 <sharma.akash028@gmail.com>
2017-10-28 13:05:06 -0400

    60% done

--- js/script.js
+++ js/script.js
@@ -3,9 +3,64 @@ $(function () {
     //focus on first input field
     $('input')[0].focus();
     $('#color option').hide();
+    $('#payment option:eq(1)').prop('selected',true);
+    showCCInfo();
 
 });
+let checkForm = {"name_field":false,
+    "email_field":false,
+    "activity_field":false,
+    "credit_field:":false
+};
+// $('#name').change(function (e) {
+//     let usrInput = (e.target.value);
+//     console.log(usrInput.length);
+//     if (usrInput.length === 0){
+//         console.log("empty");
+//         $(this).css("border","red");
+//     }
+// });
+$('#name').keydown(function (e) {
+    let usrInput = (e.target.value);
 
+    if (usrInput === "") {
+        $(this).css("border", "5px solid #f11");
+
+    }
+    else{
+        $(this).css("border", "none");
+        checkForm.name_field = true;
+    }
+    console.log(checkForm.name_field);
+});
+
+$('#mail').keydown(function (e) {
+    let emailInput = (e.target.value);
+    console.log(emailInput);
+    let whitespace = new RegExp('\s');
+    let em = new RegExp('^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$');
+    if (whitespace.test(emailInput)){
+        console.log("Please Enter an email address");
+    }
+    else if (em.test(emailInput)){
+        console.log("Valid Email address");
+    }
+});
+function showCCInfo(){
+    $('#credit-card').show();
+    $('#paypal').hide();
+    $('#bitcoin').hide();
+}
+function showpaypalInfo(){
+    $('#credit-card').hide();
+    $('#paypal').show();
+    $('#bitcoin').hide();
+}
+function showbitcoinInfo(){
+    $('#credit-card').hide();
+    $('#paypal').hide();
+    $('#bitcoin').show();
+}
 const jobRole = document.querySelector('#title');
 const basicInfo = document.querySelectorAll('fieldset')[0];
 // jobRole.addEventListener('change',function(){
@@ -108,4 +163,23 @@ activities.addEventListener('change', (e) => {
     }
     console.log(totalDiv.textContent);
 
+});
+
+//do a case switch and add id to paypal and bitcoin divs
+// $('fieldset').eq(3).change(function (e) {
+//    console.log(e.target);
+// });
+
+$('#payment').change(function (e) {
+    const paymentOption = $( "select#payment option:checked" ).val();
+    console.log(paymentOption);
+    if (paymentOption === 'credit card' || paymentOption ==='select_method'){
+        showCCInfo();
+    }
+    else if (paymentOption === "paypal") {
+       showpaypalInfo();
+    }
+    else{
+        showbitcoinInfo();
+    }
 });
